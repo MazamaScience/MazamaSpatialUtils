4de284fbde7ca2412426d3840a2483728873b71a
Zach Stednick <stedy>
2017-05-05 08:46:56 -0700

    convertWorldBorders & convertWorldBordersSimple

--- R/convertTMWorldBorders.R
+++ R/convertTMWorldBorders.R
@@ -3,7 +3,7 @@
 #' @title Convert World Borders Shapefile
 #' @param nameOnly logical specifying whether to only return the name without creating the file
 #' @description Returns a SpatialPolygonsDataFrame for world divisions
-#' @details A world borders shapefile is downloaded and converted to a 
+#' @details A world borders shapefile is downloaded and converted to a
 #' SpatialPolygonsDataFrame with additional columns of data. The resulting file will be created
 #' in the spatial data directory which is set with \code{setSpatialDataDir()}.
 #' @return Name of the dataset being created.
@@ -14,25 +14,26 @@ convertTMWorldBorders <- function(nameOnly=FALSE) {
 
   # Use package internal data directory
   dataDir <- getSpatialDataDir()
-    
+
   # Specify the name of the dataset and file being created
   datasetName <- 'TMWorldBorders'
-  
+
   if (nameOnly) return(datasetName)
-  
+
   # Build appropriate request URL for TM World Borders data
   url <- 'http://thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip'
-  
+
   filePath <- paste(dataDir,basename(url),sep='/')
   utils::download.file(url,filePath)
   # NOTE:  This zip file has no directory so extra subdirectory needs to be created
   utils::unzip(filePath,exdir=paste0(dataDir,'/world'))
-  
+
   # Convert shapefile into SpatialPolygonsDataFrame
   # NOTE:  The 'world' directory has been created
   dsnPath <- paste(dataDir,'world',sep='/')
   shpName <- 'TM_WORLD_BORDERS-0.3'
   SPDF <- convertLayer(dsn=dsnPath,layerName=shpName)
+  SPDF$polygonID <- sapply(1:nrow(SPDF), function(x) {SPDF@polygons[[x]]@ID})
 
   # Rationalize naming:
   # * human readable full nouns with descriptive prefixes
@@ -43,27 +44,29 @@ convertTMWorldBorders <- function(nameOnly=FALSE) {
   # * longitude (decimal degrees E)
   # * latitude (decimal degrees N)
   # * area (m^2)
-  
+
   # Relabel and standardize the naming in the SpatialPolygonsDataFrame
-  names(SPDF) <- c('FIPS','countryCode','ISO3','UN_country','countryName','area','population2005','UN_region','UN_subregion','longitude','latitude')
-  
+  names(SPDF) <- c('FIPS','countryCode','ISO3','UN_country','countryName',
+                   'area','population2005','UN_region','UN_subregion',
+                   'longitude','latitude', 'polygonID')
+
   # Rationalize units:
-  # * SI  
+  # * SI
   # NOTE:  Area seems to be in units of (10 km^2). Convert these to m^2
   SPDF$area <- SPDF$area * 1e7
-  
+
   # Group polygons with the same identifier (countryCode)
   SPDF <- organizePolygons(SPDF, uniqueID='countryCode', sumColumns=c('area','population2005'))
   # NOTE:  This dataset already has grouped polygons
-  
+
   # Assign a name and save the data
   assign(datasetName,SPDF)
   save(list=c(datasetName),file=paste0(dataDir,'/',datasetName,'.RData'))
-  
+
   # Clean up
   unlink(filePath, force=TRUE)
   unlink(dsnPath, recursive=TRUE, force=TRUE)
-  
+
   return(invisible(datasetName))
 }
 
--- R/convertTMWorldBordersSimple.R
+++ R/convertTMWorldBordersSimple.R
@@ -3,10 +3,10 @@
 #' @title Convert (Simple) World Borders Shapefile
 #' @param nameOnly logical specifying whether to only return the name without creating the file
 #' @description Returns a SpatialPolygonsDataFrame for a simple world divisions
-#' @details A world borders shapefile is downloaded and converted to a 
+#' @details A world borders shapefile is downloaded and converted to a
 #' SpatialPolygonsDataFrame with additional columns of data. The resulting file will be created
 #' in the package \code{SpatialDataDir} which is set with \code{setSpatialDataDir()}.
-#' 
+#'
 #' This shapefile is a greatly simplified version of the TMWorldBorders shapefile and is especially suited
 #' for spatial searches. This is the default dataset used in \code{getCountry()} and \code{getCountryCode()}.
 #' Users may wish to use a higher resolution dataset when plotting.
@@ -18,25 +18,26 @@ convertTMWorldBordersSimple <- function(nameOnly=FALSE) {
 
   # Use package internal data directory
   dataDir <- getSpatialDataDir()
-    
+
   # Specify the name of the dataset and file being created
   datasetName <- 'TMWorldBordersSimple'
-  
+
   if (nameOnly) return(datasetName)
-  
+
   # Build appropriate request URL for TM World Borders data
   url <- 'http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip'
-  
+
   filePath <- paste(dataDir,basename(url),sep='/')
   utils::download.file(url,filePath)
   # NOTE:  This zip file has no directory so extra subdirectory needs to be created
   utils::unzip(filePath,exdir=paste0(dataDir,'/world'))
-    
+
   # Convert shapefile into SpatialPolygonsDataFrame
   # NOTE:  The 'world' directory has been created
   dsnPath <- paste(dataDir,'world',sep='/')
   shpName <- 'TM_WORLD_BORDERS_SIMPL-0.3'
   SPDF <- convertLayer(dsn=dsnPath,layerName=shpName)
+  SPDF$polygonID <- sapply(1:nrow(SPDF), function(x) {SPDF@polygons[[x]]@ID})
 
   # Rationalize naming:
   # * human readable full nouns with descriptive prefixes
@@ -47,28 +48,28 @@ convertTMWorldBordersSimple <- function(nameOnly=FALSE) {
   # * longitude (decimal degrees E)
   # * latitude (decimal degrees N)
   # * area (m^2)
-  
+
   # Relabel and standardize the naming in the SpatialPolygonsDataFrame
   names(SPDF) <- c('FIPS','countryCode','ISO3','UN_country','countryName','area','population2005',
-                   'UN_region','UN_subregion','longitude','latitude')
-  
+                   'UN_region','UN_subregion','longitude','latitude', 'polygonID')
+
   # Rationalize units:
-  # * SI  
+  # * SI
   # NOTE:  Area seems to be in units of (10 km^2). Convert these to m^2
   SPDF$area <- SPDF$area * 1e7
-  
+
   # Group polygons with the same identifier (countryCode)
   SPDF <- organizePolygons(SPDF, uniqueID='countryCode', sumColumns=c('area','population2005'))
   # NOTE:  This dataset already has grouped polygons
-  
+
   # Assign a name and save the data
   assign(datasetName,SPDF)
   save(list=c(datasetName),file=paste0(dataDir,'/',datasetName,'.RData'))
-  
+
   # Clean up
   unlink(filePath, force=TRUE)
   unlink(dsnPath, recursive=TRUE, force=TRUE)
-  
+
   return(invisible(datasetName))
 }
 
