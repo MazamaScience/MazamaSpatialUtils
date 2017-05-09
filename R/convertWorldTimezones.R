a57aa12047bccb63aa947024833c7d636cf82731
Zach Stednick <stedy>
2017-05-08 21:27:11 -0700

    polygonID added, also fixed issue with no column named PID

--- R/convertWorldTimezones.R
+++ R/convertWorldTimezones.R
@@ -11,12 +11,12 @@
 #' \preformatted{
 #' > WorldTimezones@@data$timezone[is.na(WorldTimezones$countryCode)]
 #' [1] "Europe/Zagreb"         "Europe/Vatican"        "America/Coral_Harbour"
-#' [4] "Arctic/Longyearbyen"   "uninhabited"           "America/Kralendijk"   
+#' [4] "Arctic/Longyearbyen"   "uninhabited"           "America/Kralendijk"
 #' [7] "Europe/Jersey"         "Europe/Bratislava"     "America/St_Barthelemy"
-#' [10] "Europe/Ljubljana"      "Europe/Mariehamn"      "Europe/Podgorica"     
-#' [13] "Europe/Isle_of_Man"    "Europe/Guernsey"       "Europe/San_Marino"    
+#' [10] "Europe/Ljubljana"      "Europe/Mariehamn"      "Europe/Podgorica"
+#' [13] "Europe/Isle_of_Man"    "Europe/Guernsey"       "Europe/San_Marino"
 #' [16] "Europe/Skopje"         "Europe/Sarajevo"       "America/Lower_Princes"
-#' [19] "America/Marigot"       "Africa/Juba"          
+#' [19] "America/Marigot"       "Africa/Juba"
 #' }
 #' @return Name of the dataset being created.
 #' @seealso setSpatialDataDir
@@ -25,43 +25,44 @@ convertWorldTimezones <- function(nameOnly=FALSE) {
 
   # Use package internal data directory
   dataDir <- getSpatialDataDir()
-  
+
   # Specify the name of the file being created
   datasetName <- 'WorldTimezones'
-  
+
   if (nameOnly) return(datasetName)
-  
+
   # Build appropriate request URL for world timezones
   url <- "http://efele.net/maps/tz/world/tz_world.zip"
-  
+
   filePath <- paste(dataDir,basename(url),sep='/')
   utils::download.file(url,filePath)
   utils::unzip(filePath,exdir=dataDir)
-  
+
   # Convert shapefile into SpatialPolygonsDataFrame
   dsnPath <- paste(dataDir,'world',sep='/')
   SPDF <- convertLayer(dsn=dsnPath,layerName='tz_world')
-  
+
   # Rename "TZID" to "timezone"
   names(SPDF@data) <- c('timezone')
-  
+
   # Now get additional data from Wikipedia
   wikipediaTimezoneTable <- convertWikipediaTimezoneTable()
-  
+
   # Merge the additional data onto the @data slot of the SPDF
   SPDF@data <- dplyr::left_join(SPDF@data, wikipediaTimezoneTable, by="timezone")
-  
+
   # Group polygons with the same identifier
-  SPDF <- organizePolygons(SPDF, uniqueID='PID')
+  SPDF <- organizePolygons(SPDF, uniqueID='timezone')
+  SPDF$polygonID <- sapply(1:nrow(SPDF), function(x) {SPDF@polygons[[x]]@ID})
 
   # Assign a name and save the data
   assign(datasetName,SPDF)
-  save(list=c(datasetName),file=paste0(dataDir,'/',datasetName,'.RData'))  
-  
+  save(list=c(datasetName),file=paste0(dataDir,'/',datasetName,'.RData'))
+
   # Clean up
   unlink(filePath, force=TRUE)
   unlink(dsnPath, recursive=TRUE, force=TRUE)
-  
+
   return(invisible(datasetName))
 }
 
