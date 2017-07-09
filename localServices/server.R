caa631bad6c8f9a3d05e9f428f1f490b2aff62bb
Zach Stednick <stedy>
2017-07-09 16:51:11 -0700

    Example with WA HUC data and example file

--- localServices/server.R
+++ localServices/server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(MazamaSpatialUtils)
-library(ggplot2)
+library(sp)
+
+#this is a hard-coded link for dev purposes
 setSpatialDataDir("~/Data/Spatial/")
 
 function(input, output){
@@ -11,6 +13,26 @@ function(input, output){
       return(NULL)
     inputData <- read.csv(inFile$datapath)
     inputData <- subset(inputData, longitude < -80)
-    plot(inputData$longitude, inputData$latitude)
+
+    loadSpatialData("WBDHU8")
+    waHUC8 <- subset(WBDHU8, stateCode == 'WA')
+
+    forPlot <- summarizeByPolygon(inputData$longitude, inputData$latitude, value = inputData$age,
+                                  SPDF = waHUC8, polygonName = "HUCName", FUN = mean)
+    forPlot <- na.omit(forPlot)
+
+    # Get the correct plot order
+    plotOrder <- waHUC8$HUCName[waHUC8$HUCName %in% forPlot$polygonName]
+    plotOrder <- as.data.frame(plotOrder)
+    names(plotOrder) <- "polygonName"
+    plotDF <- dplyr::left_join(plotOrder, forPlot, by='polygonName')
+
+    # Plot colors by quantiles
+    breaks <- quantile(forPlot$summaryValue)
+    colIndexes <- .bincode(plotDF$summaryValue, breaks)
+    colors <- RColorBrewer::brewer.pal(4, 'Blues')
+    cols <- colors[colIndexes]
+
+    plot(waHUC8, col = cols)
   })
 }
