7bb20d563e8354e80b6a146b2ad482a2fa6ddb84
Zach Stednick <stednick@gmail.com>
2017-08-30 12:50:45 -0700

    code restructure for speed and better scoping

--- localServices/server.R
+++ localServices/server.R
@@ -5,39 +5,34 @@ library(sp)
 #this is a hard-coded link for dev purposes
 setSpatialDataDir("~/Data/Spatial/")
 
+loadSpatialData('NaturalEarthAdm1')
+wa_outline <- subset(NaturalEarthAdm1, countryName == "United States" & stateCode == "WA")
+
 function(input, output, session){
   my_data <- reactive({
     inFile <- input$file1
-
     req(inFile)
-    inputData <- read.csv(inFile$datapath, sep = input$sep, quote = input$quote)
-    vars <- names(inputData)
-
-    vars <- vars[vars != c('latitude', 'longitude')]
-    updateSelectInput(session, "columns","Select Columns", choices = vars)
-
-    loadSpatialData(input$SPDF)
-    sessionSPDF <- subset(eval(parse(text = input$SPDF)), stateCode == 'WA')
-
-    forPlot <- summarizeByPolygon(inputData$longitude, inputData$latitude,
-                                  value = inputData[, 3],
-                                  SPDF = sessionSPDF, FUN = eval(parse(text = input$FUN)))
-    forPlot[is.na(forPlot)] <- 0
-    forPlot
+    inputData <- reactiveFileReader(10000, session, filePath = inFile$datapath, read.csv,
+                                    sep = input$sep, quote = input$quote)
+    inputData()
   })
 
   output$myPlot <- renderPlot({
     loadSpatialData(input$SPDF)
     sessionSPDF <- subset(eval(parse(text = input$SPDF)), stateCode == 'WA')
+    forPlot <- summarizeByPolygon(my_data()$longitude, my_data()$latitude,
+                                  value = my_data()[, 3],
+                                  SPDF = sessionSPDF, FUN = eval(parse(text = input$FUN)))
+    forPlot[is.na(forPlot)] <- 0
 
     # Get the correct plot order
-    plotOrder <- sessionSPDF$polygonID[sessionSPDF$polygonID %in% my_data()$polygonID]
+    plotOrder <- sessionSPDF$polygonID[sessionSPDF$polygonID %in% forPlot$polygonID]
     plotOrder <- as.data.frame(plotOrder)
     names(plotOrder) <- "polygonID"
-    plotDF <- dplyr::left_join(plotOrder, my_data(), by='polygonID')
+    plotDF <- dplyr::left_join(plotOrder, forPlot, by='polygonID')
 
     # Plot colors by quantiles
-    breaks <- quantile(subset(my_data(), summaryValue > 0)$summaryValue)
+    breaks <- quantile(subset(forPlot, summaryValue > 0)$summaryValue)
     colIndexes <- .bincode(plotDF$summaryValue, breaks)
     colors <- RColorBrewer::brewer.pal(4, 'Blues')
     cols <- colors[colIndexes]
@@ -47,7 +42,6 @@ function(input, output, session){
       inFile <- input$file1
       req(inFile)
       inputData <- read.csv(inFile$datapath, sep = input$sep, quote = input$quote)
-
       points(inputData$longitude, inputData$latitude, pch = 2)
       legend("topright", legend = names(breaks)[1:4], fill = colors, title = "Density by area")
       title(paste(parse(text=input$SPDF), "with", parse(text=input$FUN), "function"))
@@ -57,8 +51,6 @@ function(input, output, session){
       req(inFile)
       inputData <- read.csv(inFile$datapath, sep = input$sep, quote = input$quote)
 
-      loadSpatialData('NaturalEarthAdm1')
-      wa_outline <- subset(NaturalEarthAdm1, countryName == "United States" & stateCode == "WA")
       plot(wa_outline)
       points(inputData$longitude, inputData$latitude, pch = 2)
     }
@@ -74,7 +66,12 @@ function(input, output, session){
     loadSpatialData(input$SPDF)
     sessionSPDF <- subset(eval(parse(text = input$SPDF)), stateCode == 'WA')
 
-    plotOrder <- sessionSPDF$polygonID[sessionSPDF$polygonID %in% my_data()$polygonID]
+    forPlot <- summarizeByPolygon(my_data()$longitude, my_data()$latitude,
+                                  value = my_data()[, 3],
+                                  SPDF = sessionSPDF, FUN = eval(parse(text = input$FUN)))
+    forPlot[is.na(forPlot)] <- 0
+
+    plotOrder <- sessionSPDF$polygonID[sessionSPDF$polygonID %in% forPlot$polygonID]
     plotOrder <- as.data.frame(plotOrder)
     names(plotOrder) <- "polygonID"
 
@@ -85,11 +82,11 @@ function(input, output, session){
       inFile <- input$file1
       req(inFile)
       inputData <- read.csv(inFile$datapath, sep = input$sep, quote = input$quote)
-      plotDF <- merge(inputData, my_data())
+      plotDF <- merge(inputData, forPlot)
       plotDF
     }
     else{
-    plotDF <- dplyr::left_join(plotOrder, my_data(), by='polygonID')
+    plotDF <- dplyr::left_join(plotOrder, forPlot, by='polygonID')
     plotDF
     }
   })
