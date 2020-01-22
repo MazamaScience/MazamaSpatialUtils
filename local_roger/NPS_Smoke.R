7eb0f7b637d9df868db3316b7ee55961f4e66008
lagerratrobe <randre@gmail.com>
2020-01-22 12:54:27 -0800

    Completed basic spatial analysis to intersect all monitor locations with all NPS lands.

--- local_roger/NPS_Smoke.R
+++ local_roger/NPS_Smoke.R
@@ -9,18 +9,22 @@
 # Plan of attack
 # --------------
 # [X] 1. Get all of the monitor locations
-# [ ] 2. Get all of the National Parks
+# [X] 2. Get all of the National Parks
 # [ ] 3. Determine what parks have a monitor station in them (or nearby?)
-#   - [ ] Are there in fact any parks with monitors?
+#   - [X] Are there in fact any parks with monitors?
 #   - [ ] How many of the total parks have a station?
 #   - [ ] Is this a meaningful data set to look at?
-# [ ] 4. Isolate the in_park stations
+# [X] 4. Isolate the in_park stations
 # [X] 5. Pull the in-park monitor data for the months of Jun - Sep 2018
 # [ ] 6. Figure out which parks had alot of smoke
 # [ ] 7. Figure out which park was the worst
+# 
+# May need to pick a specific park that has monitors, like Glacier, and analyze
+# by time instead.
 
 library(PWFSLSmoke)
 library(MazamaSpatialUtils)
+library(rmapshaper)
 
 setSpatialDataDir("~/Data/Spatial")
 loadSpatialData("HIFLDFederalLands")
@@ -50,3 +54,47 @@ monitor_spdf <- SpatialPointsDataFrame(coords = xy, data = monitor_location_df,
 # quick and dirty plot of CONUS with monitor locations on it
 plot(conus_states)
 plot(monitor_spdf, col = "red", add = TRUE)
+
+# See lots of data points in Canada that I don't want.  Going to remove them by
+# using the countryCode in the the monitor data
+conus_monitor_spdf <- subset(monitor_spdf, monitor_spdf@data$countryCode == 'US')
+plot(conus_monitor_spdf, col = "forestgreen", add = TRUE)
+
+# Also see that these state borders are too detailed. Simplifying to 1%
+conus_states_01 <- rmapshaper::ms_simplify(conus_states, 0.01)
+conus_states_01@data$rmapshaperid <- NULL
+
+# ---- Get the National Parks out of the HIFLDFederalLands data ----------------
+loadSpatialData("HIFLDFederalLands")
+nps_lands <- subset(HIFLDFederalLands, HIFLDFederalLands@data$agencyCode == "NPS")
+
+# Quick and dirty plot of what we have so far
+dev.off()
+plot(conus_states_01, col = "grey90")
+plot(nps_lands, col = "goldenrod", add = TRUE)
+plot(conus_monitor_spdf, col = "forestgreen", add = TRUE)
+
+#reset the projections for all of our spatial data to 
+# USA_Contiguous_Lambert_Conformal_Conic, esri:102004
+
+lcc_conus_states <- spTransform(conus_states_01, CRS("+init=esri:102004"))
+lcc_nps_lands <- spTransform(nps_lands, CRS("+init=esri:102004"))
+lcc_monitor <- spTransform(conus_monitor_spdf, CRS("+init=esri:102004") )
+
+# Let's seee if there are any monitors that intersect with NPS lands
+lcc_nps_monitor <- lcc_monitor[lcc_nps_lands,]
+
+# Replotting
+dev.off()
+plot(lcc_conus_states, col = "grey90")
+plot(lcc_nps_lands, col = "goldenrod", add = TRUE)
+plot(lcc_monitor, col = "forestgreen", add = TRUE)
+plot(lcc_nps_monitor, col = "red", add = TRUE)
+
+# There are some, but many of the larger parks, like Olympic in WA are missing
+#  How many monitors are there inside parks?  Ans = 39
+length(lcc_nps_monitor)
+
+# Going to have to think about this a bit.  I could do a buffered search and tie 
+# nearby monitor locations to parks that don't have any.  Or I could switch to 
+# using a different feature type from the Fed Lands data.
