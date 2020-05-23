4d98192c0e18ad08e415a5c0a45ff7f7b4c8cc23
tinaisat <63061577+tinaisat@users.noreply.github.com>
2020-05-22 17:42:38 -0700

    updated to newer version of shapefile

--- R/convertWeatherZones.R
+++ R/convertWeatherZones.R
@@ -12,6 +12,8 @@
 #' @references \url{https://www.weather.gov/gis/PublicZones}
 #' @seealso setSpatialDataDir
 
+# ----- Setup ----------------------------------------------------------------
+
 convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
   
   # Use package internal data directory
@@ -21,20 +23,31 @@ convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
   datasetName <- 'weatherZones'
   
   if (nameOnly) return(datasetName)
+
+  # ----- Get the data ---------------------------------------------------------
   
   # Build appropriate request URL for TM World Borders data
-  url <- "https://www.weather.gov/source/gis/Shapefiles/WSOM/z_02oc18.zip"
+  url <- "https://www.weather.gov/source/gis/Shapefiles/WSOM/z_03mr20.zip"
   filePath <- file.path(dataDir, "weatherZones.zip")
   utils::download.file(url,filePath)
+  
+  # ----- Convert to SPDF ------------------------------------------------------
   
   # NOTE:  This zip file has no directory so extra subdirectory needs to be created
   utils::unzip(filePath,exdir=file.path(dataDir,'weatherZones'))
   dsnPath <- file.path(dataDir,'weatherZones')
-  SPDF <- convertLayer(dsn=dsnPath, layerName='z_02oc18')
+  SPDF <- convertLayer(dsn=dsnPath, layerName='z_03mr20')
   
-  # > names(weather_05)
-  # [1] "STATE"      "CWA"        "TIME_ZONE"  "FE_AREA"    "ZONE"       "NAME"       "STATE_ZONE" "LON"       
-  # [9] "LAT"        "SHORTNAME" 
+  # > names(SPDF)
+  # [1] "STATE"      "CWA"        "TIME_ZONE"  "FE_AREA"    "ZONE"       "NAME"       "STATE_ZONE"
+  # [8] "LON"        "LAT"        "SHORTNAME"  "InPoly_FID" "SimPgnFlag" "MaxSimpTol" "MinSimpTol"
+  
+  # > head(SPDF@data)
+  # STATE CWA TIME_ZONE FE_AREA ZONE     NAME STATE_ZONE      LON    LAT SHORTNAME InPoly_FID SimPgnFlag MaxSimpTol MinSimpTol
+  # 0    GU GUM         J    <NA>  013 Kayangel      GU013 134.7129 8.0736  Kayangel          1          0      1e-04      1e-04
+  # 1    GU GUM         J    <NA>  011    Koror      GU011 134.5351 7.4442     Koror          2          0      1e-04      1e-04
+  # 2    GU GUM         F    <NA>  041  Pohnpei      GU041 158.2252 6.8807   Pohnpei          3          0      1e-04      1e-04
+  
   
   # Relabel and standardize the naming in the SpatialPolygonsDataFrame
   
@@ -47,7 +60,8 @@ convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
   # NOTE:  plot(subset(WorldTimezones, timezone == "America/Anchorage"), border = "red", add = TRUE)
   # NOTE:  plot(subset(WorldTimezones, timezone == "America/Nome"), border = "blue", add = TRUE)
   
-
+  # ----- Select useful columns and rename -------------------------------------
+  
   SPDF@data <- dplyr::select(SPDF@data,
                       stateCode = .data$STATE,
                       weatherForecastOffice = .data$CWA,
@@ -57,6 +71,7 @@ convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
                       longitude = .data$LON,
                       latitude = .data$LAT)
   
+  # ----- Organize polygons ----------------------------------------------------
   
   # Organize polygons
   duplicated <- SPDF$zoneID[duplicated(SPDF$zoneID)]
@@ -71,11 +86,16 @@ convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
   }
   SPDF@data <- data
   
+  
+  # ----- Name and save the data -----------------------------------------------
+  
   # Assign name and save the data
   assign(datasetName,SPDF)
   save(list=datasetName, file = paste0(dataDir,"/",datasetName, '.RData'))
   rm(list=datasetName)
   
+  
+  # ----- Simplify -------------------------------------------------------------
   
   if ( simplify ) {
     
@@ -85,6 +105,8 @@ convertWeatherZones <- function(nameOnly = FALSE, simplify = TRUE) {
     save(list = datasetName_05, file = paste0(dataDir, "/", datasetName_05, ".RData"))
     
   }
+  
+  # ----- Clean up and return --------------------------------------------------
   
   return(invisible(datasetName))  
   
