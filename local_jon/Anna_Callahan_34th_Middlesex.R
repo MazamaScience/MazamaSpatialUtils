d46502e81edfaf1e945af497a7845390caca9132
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-08-17 15:32:28 -0700

    docs: voting districts example in local_jon/

--- local_jon/Anna_Callahan_34th_Middlesex.R
+++ local_jon/Anna_Callahan_34th_Middlesex.R
@@ -18,21 +18,49 @@ plot(spdf)
 # x -71.13464 -71.07943
 # y  42.38736  42.41815
 
+Anna_lat <- 42.4007906
+Anna_lon <- -71.1062108
+
+border <- 0.001
 
 # From: https://cfss.uchicago.edu/notes/raster-maps-with-ggmap/
 
+library(ggplot2)
 library(ggmap)
+
+spdf_tidy <- broom::tidy(spdf)
 
 medford_stamen <- get_stamenmap(
   bbox = c(
-    left = -71.13464,
-    bottom = 42.38736,
-    right = -71.07943,
-    top = 42.41815
+    left = -71.13464 - border,
+    bottom = 42.38736 - border,
+    right = -71.07943 + border,
+    top = 42.41815 + border
   ),
   zoom = 17
 )
 
-ggmap(medford_stamen)
+gg <-
+  ggmap(medford_stamen) +
+  geom_polygon(
+    data = spdf_tidy,
+    aes(x = long, y = lat),
+    color = 'white',
+    alpha = 0.2,
+    size = 1,
+    fill = 'transparent'
+  ) +
+  annotate(
+    geom = "point",
+    x = Anna_lon,
+    y = Anna_lat,
+    #color = 'red',
+    #size = 4
+    shape = 24,
+    size = 2,
+    fill = "red"
+  )
+
+print(gg)
 
 # Manually exported as pdf.
