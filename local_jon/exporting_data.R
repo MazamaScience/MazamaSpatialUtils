3b18657952af4b135ea5113a8103abb24ba266c5
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2025-09-29 14:28:00 -0700

    tweak to local_jon/exporting_data.R

--- local_jon/exporting_data.R
+++ local_jon/exporting_data.R
@@ -31,13 +31,21 @@ for (datasetName in datasetNames) {
 
   dataset_name <- datasetName %>% snakecase::to_snake_case()
 
-  SPDF <- get(loadSpatialData(paste0(datasetName, ".rda")))
+  SPDF <-
+    get(loadSpatialData(paste0(datasetName, ".rda"))) %>%
+    # Reproject to World Geodetic System: https://epsg.io/4326
+    sf::st_transform(sf::st_crs(4326))
+
+  # Convert column names to snake_case
   column_names <- colnames(SPDF) %>% snakecase::to_snake_case()
   colnames(SPDF) <- column_names
 
   # Drop any duplicates we may have created
   keepers <- !duplicated(column_names)
   SPDF <- SPDF[,keepers]
+  # Replace any existing 'id' with 'polygon_id', then remove 'polygon_id'
+  SPDF$id <- SPDF$polygon_id
+  SPDF$polygon_id <- NULL
 
   sf::st_write(
     obj = SPDF,
